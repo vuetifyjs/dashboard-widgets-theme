5252ae450a405099ad7855a7065fb362297470e2
abdelouahed oumoussa <abdelwahd.oumouss@gmail.com>
2025-08-09 16:35:59 +0100

    chore(layouts): using vite-plugin-vue-layouts-next

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,10 @@
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
-
-// Composables
-// @ts-ignore
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  extendRoutes: setupLayouts,
+  routes: setupLayouts(routes),
 })
 
 export default router
